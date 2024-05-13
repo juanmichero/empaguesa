4282dde72f68381d0a3e25d76cad3b0453b98701
juanmichero <125592627+juanmichero@users.noreply.github.com>
2024-05-13 10:42:41 -0300

    Add files via upload

---
+++ Solucion.hs
@@ -0,0 +1,75 @@
+module Solucion where
+import Data.Char
+-- No se permite agrear nuevos imports
+-- Sólo está permitido usar estas funciones:
+-- https://campus.exactas.uba.ar/pluginfile.php/557895/mod_resource/content/1/validas_tp.pdf
+
+
+-- Completar!
+-- Nombre de grupo: {}
+-- Integrante1: { DNI1,apellidoYNombre1}
+-- Integrante2: { DNI2,apellidoYNombre2}
+-- Integrante3: { DNI3,apellidoYNombre3}
+-- Integrante4: { DNI4,apellidoYNombre4}
+-- Integrantes que abandonaron la materia: {En caso que haya abandonado la materia algún
+                        -- integrante, completar con los dni y apellidos, sino dejar vacío}
+
+-- EJ 1
+esMinuscula :: Char -> Bool
+esMinuscula _ = True
+
+-- EJ 2
+letraANatural :: Char -> Int
+letraANatural _ = 1
+
+-- EJ 3
+desplazar :: Char -> Int -> Char
+desplazar _ _ = 'd'
+
+-- EJ 4
+cifrar :: String -> Int -> String
+cifrar _ _ = "frpsxwdflrq"
+
+-- EJ 5
+descifrar :: String -> Int -> String
+descifrar _ _ = "computacion"
+
+-- EJ 6
+cifrarLista :: [String] -> [String]
+cifrarLista _ = ["compu", "mbcp", "kpvtq"]
+
+-- EJ 7
+frecuencia :: String -> [Float]
+frecuencia _ = [16.666668,0.0,0.0,0.0,16.666668,0.0,0.0,0.0,0.0,0.0,0.0,33.333336,0.0,0.0,0.0,0.0,0.0,16.666668,0.0,16.666668,0.0,0.0,0.0,0.0,0.0,0.0]
+
+-- Ej 8
+cifradoMasFrecuente :: String -> Int -> (Char, Float)
+cifradoMasFrecuente _ _ = ('o', 33.333336)
+
+-- EJ 9
+esDescifrado :: String -> String -> Bool
+esDescifrado _ _ = False
+
+-- EJ 10
+todosLosDescifrados :: [String] -> [(String, String)]
+todosLosDescifrados _ = [("compu", "frpsx"), ("frpsx", "compu")]
+
+-- EJ 11
+expandirClave :: String -> Int -> String
+expandirClave _ _ = "compucom"
+
+-- EJ 12
+cifrarVigenere :: String -> String -> String
+cifrarVigenere _ _ = "kdueciirqdv"
+
+-- EJ 13
+descifrarVigenere :: String -> String -> String
+descifrarVigenere _ _ = "computacion"
+
+-- EJ 14
+peorCifrado :: String -> [String] -> String
+peorCifrado _ _ = "asdef"
+
+-- EJ 15
+combinacionesVigenere :: [String] -> [String] -> String -> [(String, String)]
+combinacionesVigenere _ _ _ = [("hola", "b")]
