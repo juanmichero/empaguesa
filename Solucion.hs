c9c37c963e3356b9369ffd620afd58daac925aaa
Pyk3i <165915733+Pyk3i@users.noreply.github.com>
2024-05-19 19:43:50 -0300

    Update Solucion.hs

--- Solucion.hs
+++ Solucion.hs
@@ -205,21 +205,66 @@ ciclo frase1 frase2 n
     |n < 27 = ciclo frase1 (cifrar frase2 1) (n+1)
     |otherwise = False
 
--- EJ 10
+-- EJ 10 
 todosLosDescifrados :: [String] -> [(String, String)]
-todosLosDescifrados _ = [("compu", "frpsx"), ("frpsx", "compu")]
+todosLosDescifrados [] = []
+todosLosDescifrados lista = descifradosLista lista 0
+
+descifradosLista :: [String] -> Int -> [(String, String)]
+descifradosLista lista n 
+    |n == length lista = []
+    |cifradoEnLista (head lista) (tail lista) = tuplaCifrado : descifradosLista rotarLista (n+1)
+    |otherwise = descifradosLista rotarLista (n+1)
+        where tuplaCifrado = (head lista, cifrado (head lista) (tail lista))
+              rotarLista = (tail lista) ++ [(head lista)]
+
+cifrado :: String -> [String] -> String -- Dado un String y una lista, da el cifrado o descifrado de cualquiera de sus pasos, de no haberlo, da un espacio en blanco
+cifrado frase [] = ""
+cifrado frase lista
+    |esDescifrado frase (head lista) = head lista
+    |otherwise = cifrado frase (tail lista)
+
+cifradoEnLista :: String -> [String] -> Bool -- Verifica que el cifrado o descifrado de una palabra esté dentro de la lista 
+cifradoEnLista _ [] = False
+cifradoEnLista frase lista
+    |esDescifrado frase (head lista) = True
+    |otherwise = cifradoEnLista frase (tail lista)
 
 -- EJ 11
 expandirClave :: String -> Int -> String
-expandirClave _ _ = "compucom"
+expandirClave "" _ = ""
+expandirClave frase n
+    |length frase > n = eliminarExceso frase n
+    |otherwise = expandirClave (frase ++ frase) n
+
+
+eliminarExceso :: String -> Int -> String -- Toma una palabra y recortar los últimos n caracteres
+eliminarExceso "" _ = ""
+eliminarExceso frase n
+    |n == 0 = []
+    |otherwise = head frase : eliminarExceso (tail frase) (n-1) 
+
+{--rotarFrase :: String -> Int -> String
+rotarFrase frase 0 = frase 
+rotarFrase frase n = rotarFrase (tail frase ++ [(head frase)]) (n-1)--}
 
 -- EJ 12
 cifrarVigenere :: String -> String -> String
-cifrarVigenere _ _ = "kdueciirqdv"
+cifrarVigenere ""  ""   = []
+cifrarVigenere ""  _    = ""
+cifrarVigenere frase "" = frase
+cifrarVigenere frase1 frase2 = primeraLetra : cifrarSiguienteLetra
+    where primeraLetra = desplazar (head frase1) (letraANatural(head frase2))
+          cifrarSiguienteLetra = cifrarVigenere (tail frase1) (tail (expandirClave frase2 (length frase1)))
 
 -- EJ 13
 descifrarVigenere :: String -> String -> String
-descifrarVigenere _ _ = "computacion"
+descifrarVigenere "" ""    = []
+descifrarVigenere "" _     = ""
+descifrarVigenere frase "" = frase
+descifrarVigenere frase1 frase2 = primeraLetra : descifrarSiguienteLetra
+    where primeraLetra = desplazar (head frase1) ((letraANatural(head frase2))*(-1))
+          descifrarSiguienteLetra = descifrarVigenere (tail frase1) (tail (expandirClave frase2 (length frase1)))
 
 -- EJ 14
 peorCifrado :: String -> [String] -> String
