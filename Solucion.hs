b98bbb609ac40d9600b20493d36c5ceafe92fc9d
Favio Ontón <97208954+BiyoDev@users.noreply.github.com>
2024-05-20 01:36:38 -0300

    Update Solucion.hs

--- Solucion.hs
+++ Solucion.hs
@@ -12,15 +12,15 @@ esMinuscula c
     
 --2
 letraANatural :: Char -> Int
-letraANatural c = ord c
+letraANatural c = ord c - 97
 
 --3
 desplazar :: Char -> Int -> Char
-desplazar c n 
-    |ordc + n >= 97 && ordc + n <= 122 = chr (ordc + n)
-    |otherwise = chr (ordc-26 + (mod n 25))
-    where 
-        ordc = letraANatural c 
+desplazar c n
+    |not (esMinuscula c) = c 
+    |otherwise = chr(97 + mod (valorLetra + n) 26)
+    where
+        valorLetra = letraANatural c
         
 --4
 cifrar :: String -> Int -> String
@@ -33,15 +33,16 @@ descifrar [] _ = []
 descifrar (x:xs) n = desplazar x (-n) : descifrar xs n
 
 
---------------------------------------------------
-
-
 
 
 -- EJ 6
-cifrarLista :: [String] -> [String]
-cifrarLista _ = ["compu", "mbcp", "kpvtq"]
 
+cifrarLista :: [String] -> [String]
+cifrarLista lista = cifrarListaAux lista (length lista) 0
+cifrarListaAux :: [String] -> Int -> Int -> [String]
+cifrarListaAux [] _ _  = []
+cifrarListaAux (l:ls) longitud it
+    |it < longitud = cifrar l it : cifrarListaAux ls longitud (it+1)
 -- EJ 7
 frecuencia :: String -> [Float]
 frecuencia _ = [16.666668,0.0,0.0,0.0,16.666668,0.0,0.0,0.0,0.0,0.0,0.0,33.333336,0.0,0.0,0.0,0.0,0.0,16.666668,0.0,16.666668,0.0,0.0,0.0,0.0,0.0,0.0]
@@ -59,8 +60,18 @@ todosLosDescifrados :: [String] -> [(String, String)]
 todosLosDescifrados _ = [("compu", "frpsx"), ("frpsx", "compu")]
 
 -- EJ 11
+
+
+expandirClaveAux :: String -> String -> Int -> Int -> String
+expandirClaveAux frase (x:xs) k n
+    |k==n = ""
+    |xs == [] = x:expandirClaveAux frase frase (k+1) n
+    |otherwise = x:expandirClaveAux frase xs (k+1) n
+
+
 expandirClave :: String -> Int -> String
-expandirClave _ _ = "compucom"
+expandirClave frase n = expandirClaveAux frase frase 0 n  
+
 
 -- EJ 12
 cifrarVigenere :: String -> String -> String
@@ -77,3 +88,4 @@ peorCifrado _ _ = "asdef"
 -- EJ 15
 combinacionesVigenere :: [String] -> [String] -> String -> [(String, String)]
 combinacionesVigenere _ _ _ = [("hola", "b")]
+
