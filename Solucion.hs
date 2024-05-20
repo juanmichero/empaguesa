9a0649a6d09481c9ca140f68ca0246e75cd15180
Favio Ontón <97208954+BiyoDev@users.noreply.github.com>
2024-05-20 18:08:51 -0300

    Update Solucion.hs

--- Solucion.hs
+++ Solucion.hs
@@ -4,46 +4,18 @@ import Data.Char
 -- Sólo está permitido usar estas funciones:
 -- https://campus.exactas.uba.ar/pluginfile.php/557895/mod_resource/content/1/validas_tp.pdf
 
---1
-esMinuscula :: Char -> Bool
-esMinuscula c
-    |ord c >= 97 && ord c <= 122 = True
-    |otherwise = False
-    
---2
-letraANatural :: Char -> Int
-letraANatural c = ord c - 97
+--Funciones auxiliares
 
---3
-desplazar :: Char -> Int -> Char
-desplazar c n
-    |not (esMinuscula c) = c 
-    |otherwise = chr(97 + mod (valorLetra + n) 26)
-    where
-        valorLetra = letraANatural c
-        
---4
-cifrar :: String -> Int -> String
-cifrar [] _ = []
-cifrar (x:xs) n = desplazar x n : cifrar xs n
+numeroALEtra :: Int -> Char
+numeroALEtra n = chr (97 + n)
 
---5
-descifrar :: String -> Int -> String
-descifrar [] _ = []
-descifrar (x:xs) n = desplazar x (-n) : descifrar xs n
-
-
-
-
--- EJ 6
-
-cifrarLista :: [String] -> [String]
-cifrarLista lista = cifrarListaAux lista (length lista) 0
+--del ej 6
 cifrarListaAux :: [String] -> Int -> Int -> [String]
 cifrarListaAux [] _ _  = []
-cifrarListaAux (l:ls) longitud it
-    |it < longitud = cifrar l it : cifrarListaAux ls longitud (it+1)
--- EJ 7
+cifrarListaAux (l:ls) longitud k
+    |k < longitud = cifrar l k : cifrarListaAux ls longitud (k+1)
+
+--del ej 7
 pertenece ::(Eq t)=>t -> [t] -> Bool
 pertenece _ [] = False
 pertenece e (x:xs)
@@ -63,17 +35,50 @@ cuantasVecesAparece e (x:xs)
     |otherwise = cuantasVecesAparece e xs
 
 frecuenciaAux :: String -> Int -> Int -> [Float]
-frecuenciaAux frase k n 
+frecuenciaAux frase k n
     |k==n+1 = []
-    |pertenece letra frase = (fromIntegral(cuantasVecesAparece letra frase)*100/ fromIntegral(contarMinusculas frase) ): frecuenciaAux frase (k+1) n
-    |otherwise = 0.0 : frecuenciaAux frase (k+1) n 
+    |pertenece letra frase = (fromIntegral (cuantasVecesAparece letra frase)*100/ fromIntegral (contarMinusculas frase) ): frecuenciaAux frase (k+1) n
+    |otherwise = 0.0 : frecuenciaAux frase (k+1) n
     where
         letra = chr (97+k)
-        
+
+
+
+--1
+esMinuscula :: Char -> Bool
+esMinuscula c
+    |ord c >= 97 && ord c <= 122 = True
+    |otherwise = False
+
+--2
+letraANatural :: Char -> Int
+letraANatural c = ord c - 97
+
+--3
+desplazar :: Char -> Int -> Char
+desplazar c n
+    |not (esMinuscula c) = c
+    |otherwise = chr (97 + mod (letraANatural c + n) 26)
+
+--4
+cifrar :: String -> Int -> String
+cifrar [] _ = []
+cifrar (x:xs) n = desplazar x n : cifrar xs n
+
+--5
+descifrar :: String -> Int -> String
+descifrar [] _ = []
+descifrar (x:xs) n = desplazar x (-n) : descifrar xs n
+
+--6
+cifrarLista :: [String] -> [String]
+cifrarLista lista = cifrarListaAux lista (length lista) 0
+
+--7
 frecuencia :: String -> [Float]
 frecuencia frase = frecuenciaAux frase 0 25
 
--- Ej 8
+--8
 indice :: (Eq t)=> t ->[t]-> Int
 indice e (x:xs)
     |e==x= 0
@@ -85,27 +90,25 @@ indice e (x:xs)
 maximo ::(Ord t)=> [t] -> t
 maximo [n] = n
 maximo (x:xs)
-    |x > head(xs) = maximo(x:tail(xs))
-    |otherwise = maximo xs 
+    |x > head (xs) = maximo (x:tail (xs))
+    |otherwise = maximo xs
 
 
 cifradoMasFrecuente:: String -> Int -> (Char,Float)
-cifradoMasFrecuente frase n = (chr(97 + indice(maximo(frecuencia(cifrar frase n))) (frecuencia(cifrar frase n))), maximo(frecuencia(cifrar frase n)))
-  
+cifradoMasFrecuente frase n = (chr (97 + indice (maximo (frecuencia (cifrar frase n))) (frecuencia (cifrar frase n))), maximo (frecuencia (cifrar frase n)))
 
-
--- EJ 9
+--9
 esDescifrado :: String -> String -> Bool
 esDescifrado _ _ = False
 
--- EJ 10
+-- 10
 todosLosDescifrados :: [String] -> [(String, String)]
 todosLosDescifrados _ = [("compu", "frpsx"), ("frpsx", "compu")]
 
--- EJ 11
-
+-- 11
 
 expandirClaveAux :: String -> String -> Int -> Int -> String
+expandirClaveAux "" _ _ _ = ""
 expandirClaveAux frase (x:xs) k n
     |k==n = ""
     |xs == [] = x:expandirClaveAux frase frase (k+1) n
@@ -113,12 +116,26 @@ expandirClaveAux frase (x:xs) k n
 
 
 expandirClave :: String -> Int -> String
-expandirClave frase n = expandirClaveAux frase frase 0 n  
+expandirClave frase n = expandirClaveAux frase frase 0 n
 
 
 -- EJ 12
+longitud :: String -> Int
+longitud "" = 0
+longitud (x:xs) 
+    |xs /= [] = 1 + longitud xs
+    |otherwise = 1
+
+
 cifrarVigenere :: String -> String -> String
-cifrarVigenere _ _ = "kdueciirqdv"
+cifrarVigenere frase clave
+    |tail (frase)==[] && tail (claveExpandida)==[] = []
+    |otherwise = (desplazar (head (frase)) (letraANatural (head claveExpandida))): cifrarVigenere (tail frase) (tail clave)
+    where
+        claveExpandida = expandirClave clave (longitud (frase))
+
+
+
 
 -- EJ 13
 descifrarVigenere :: String -> String -> String
@@ -131,4 +148,5 @@ peorCifrado _ _ = "asdef"
 -- EJ 15
 combinacionesVigenere :: [String] -> [String] -> String -> [(String, String)]
 combinacionesVigenere _ _ _ = [("hola", "b")]
+
 
